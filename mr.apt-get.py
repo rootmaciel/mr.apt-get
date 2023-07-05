8b47423114a3361c10db1d7789df504c3e192533
Root Maciel <65637444+rootmaciel@users.noreply.github.com>
2023-07-05 10:34:53 -0300

    Update mr.apt-get.py

--- mr.apt-get.py
+++ mr.apt-get.py
@@ -34,7 +34,7 @@ print('''\033[32m
     |34: fluxion        |39: Xprobe2       |44: dirb
     |35: okadminfinder  |40: wpscan        |45: Storm-Breaker
 
-    |46: Parcellite
+    |46: Parcellite     |47: Bluetooth
 \033[0;0m''')
 
 num = input("\033[37mEscolha um numero (0/Exit): \033[0;0m")
@@ -51,7 +51,7 @@ while num != 0:
             print("Thanks u")
             os.system(exit())
         
-        os.system("apt install -y make git figlet vlc cmatrix vim idle3 python-pip python3-pip nmap qbittorrent wireshark ettercap-graphical sslstrip dsniff driftnet aircrack-ng libclc-dev libclc-r600 mesa-opencl-icd ocl-icd-libopencl1 hashcat aircrack-ng reaver crunch perl netcat siege fcrackzip slowhttptest net-tools wget kazam openshot ffmpeg adb codeblocks binwalk dnsrecon ltrace dirb parcellite")
+        os.system("apt install -y make git figlet vlc cmatrix vim idle3 python-pip python3-pip nmap qbittorrent wireshark ettercap-graphical sslstrip dsniff driftnet aircrack-ng libclc-dev libclc-r600 mesa-opencl-icd ocl-icd-libopencl1 hashcat aircrack-ng reaver crunch perl netcat siege fcrackzip slowhttptest net-tools wget kazam openshot ffmpeg adb codeblocks binwalk dnsrecon ltrace dirb parcellite blueman")
 
     if num == '1':
         print("\n[OK]Instalando make install...")
@@ -348,9 +348,14 @@ while num != 0:
         print("\n[OK]Concluido..")
 
     if num =='46':
-    print("\n[OK]Instalando Area de Transferencia...")
-    os.system("apt-get install parcellite")
-    print("\n[OK]Concluido..")
+        print("\n[OK]Instalando Area de Transferencia...")
+        os.system("apt-get install parcellite")
+        print("\n[OK]Concluido..")
+    
+    if num =='47':
+        print("\n[OK]Instalando Bluetooth...")
+        os.system("apt-get install blueman")
+        print("\n[OK]Concluido..")    
         
     print('''\033[36m
     _______________________________________________________
@@ -384,7 +389,7 @@ while num != 0:
     |34: fluxion        |39: Xprobe2       |44: dirb
     |35: okadminfinder  |40: wpscan        |45: Storm-Breaker
 
-    |46: Parcellite
+    |46: Parcellite     |47: Bluetooth
     \033[0;0m''')
 
     num = input("\033[37mEscolha um numero (0/Exit):\033[0;0m")
